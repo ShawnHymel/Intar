a76afdc3878f99f204478aa31e01c52fa1119972
Shawn Hymel <shawn.hymel@sparkfun.com>
2015-03-30 22:48:08 -0600

    Created TagIR library template.

---
+++ Libraries/TagIR/TagIR.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file       TagIR.cpp
+ * @brief 	   Library for the Arduino-based laser tag system
+ * @author     Shawn Hymel
+ * @copyright  2015 Shawn Hymel, Nick Poole
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+#include <Arduino.h>
+ 
+#include "TagIR.h"
---
+++ Libraries/TagIR/TagIR.h
@@ -0,0 +1,25 @@
+/**
+ * @file       TagIR.h
+ * @brief 	   Library for the Arduino-based laser tag system
+ * @author     Shawn Hymel
+ * @copyright  2015 Shawn Hymel, Nick Poole
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+#ifndef TagIR_H
+#define TagIR_H
+
+#include <Arduino.h>
+
+/* Debug */
+#define DEBUG_TAGIR         0
+
+/* TagIR Class */
+class TagIR {
+public:
+
+private:
+
+};
+
+#endif // TagIR_H
